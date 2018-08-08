efbf53be6e3f0a894eb6531f7a73667ef1e548de
Jonathan Kelvin <jooh.kelvin@gmail.com>
2018-08-08 14:12:21 -0300

    Menu laterial adicionado na activity principal

--- app/src/main/java/tg2/com/local/pauloafonso/pontosturisticos/MainActivity.java
+++ app/src/main/java/tg2/com/local/pauloafonso/pontosturisticos/MainActivity.java
@@ -1,13 +1,37 @@
 package tg2.com.local.pauloafonso.pontosturisticos;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle nToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+
+        drawerLayout.addDrawerListener(nToggle);
+        nToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (nToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
