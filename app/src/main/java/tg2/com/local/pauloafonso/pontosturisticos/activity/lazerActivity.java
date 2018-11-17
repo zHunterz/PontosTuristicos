fb8f92b26588d7b022e7155b4980ebf045a78af9
Jonathan Kelvin <jooh.kelvin@gmail.com>
2018-11-17 17:39:54 -0200

    Inicio da tela principal

---
+++ app/src/main/java/tg2/com/local/pauloafonso/pontosturisticos/activity/lazerActivity.java
@@ -0,0 +1,15 @@
+package tg2.com.local.pauloafonso.pontosturisticos.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import tg2.com.local.pauloafonso.pontosturisticos.R;
+
+
+public class lazerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lazer);
+    }
+}
